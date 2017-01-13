1c15fbaf2675a4f90bbb5d3e9192f5156110c051
Hieu Nguyen <hnnguyen45@gmail.com>
2017-01-12 20:30:18 -0800

    still working and updating the income button 4

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,26 @@
+function populateIncomes(){
+
+	var totalIncomeYearlyAfterTax;
+ 	var totalIncomeYearly = $('.totalIncomeYearly').append().val().html();
+	var taxRate = 0.30 * totalIncomeYearly;//30 percent
+	totalIncomeYearlyAfterTax = taxRate;
+	$('#incomeList table tfoot').html(totalIncomeYearly);
+	$('.totalIncomeYearlyAfterTax').append().val().html();
+};
+
+function populateExpenses(){
+
+    var expensesContent = '';
+    var totalFeeMonthly = 0;
+    var totalMonthlyCost = 0;
+    var totalFeeMonthly;  
+        $('#expenseList table tr').html(totalFeeMonthly);
+        $('#totalFeeMonthly').html(totalMonthlyCost);
+    };
+
 $(document).ready(function(){
+		populateIncomes()
+		populateExpenses()
 
 	$('.deleteExpenseLink').click(function(event){
 		event.preventDefault();
